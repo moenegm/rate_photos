1ce15eff950ca5dc61e6c1066146ddd4ee2836d4
Mohamed A. Negm <mr.negm90@gmail.com>
2021-12-15 13:14:43 -0800

    adding s3 and testing something for deleting old images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+
+
     def index
         @users = User.all
     end
@@ -6,4 +10,14 @@ class UsersController < ApplicationController
     def show
         @user = current_user
     end
+
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_s3_direct_post
+            @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+        end
+        
+        def set_user
+        @user = User.find(params[:id])
+        end
 end
