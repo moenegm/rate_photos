2c3c301b654c2d7f71404321719dc64ad763f57d
Mohamed A. Negm <mr.negm90@gmail.com>
2021-12-15 14:36:05 -0800

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
 
@@ -15,9 +14,5 @@ class UsersController < ApplicationController
         # Use callbacks to share common setup or constraints between actions.
         def set_s3_direct_post
             @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-        end
-
-        def set_user
-            @user = User.find(params[:id])
         end
 end
