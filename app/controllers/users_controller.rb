17eaedb01f67ec2abe814ce355d06bcfc38c86c0
Mohamed A. Negm <mr.negm90@gmail.com>
2021-12-15 14:01:45 -0800

    what's happening

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    # before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
 
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
         def set_s3_direct_post
             @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
         end
-        
-        def set_user
-        @user = User.find(params[:id])
-        end
+
+        # def set_user
+        # @user = User.find(params[:id])
+        # end
 end
