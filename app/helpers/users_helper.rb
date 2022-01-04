744973c77c491bed9e9a59d315c02fbccbbdf87b
Mohamed A. Negm <mr.negm90@gmail.com>
2022-01-04 10:36:20 -0800

    fixed avatar bug, and edit profile required password

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+    def check_avatar(user)
+        if user.avatar.present?
+ 
+            link_to image_tag(@user.avatar, class:"avatar mb-3", radius: 0)
+        else
+            link_to image_tag("default_avatar", class:"mb-3 avatar justify-items-center", radius: 0)
+        end
+    end
 end
