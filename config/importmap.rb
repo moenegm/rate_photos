1559fc3896cb5884bc2b80da96388ebd63fb4b53
Mohamed A. Negm <mr.negm90@gmail.com>
2022-01-08 03:11:24 -0800

    more rails 7 stuff

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading-js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "trix"
-pin "@rails/axtiontext", to: "actiontext.js"
 pin "local-time" # @2.1.0
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
