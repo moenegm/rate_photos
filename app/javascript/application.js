525e38f990083693a904b7be4f6886324bde26ae
Mohamed A. Negm <mr.negm90@gmail.com>
2022-01-09 13:02:35 -0800

    Last commit in rails 7 I think

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,21 +2,17 @@
 
 //= require_tree .
 
-// import "./@rails/actionable"
-import "@rails/activestorage"
-
-import "./trix"
+import "trix"
 import "@rails/actiontext"
-
-import Rails from "./@rails/ujs"
-import Turbolinks from "./turbolinks"
-import * as ActiveStorage from "./@rails/activestorage"
-import "./channels"
-import LocalTime from "./local-time"
+import "@rails/actionable"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import LocalTime from "local-time"
 
 LocalTime.start()
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-import "trix"
-import "@rails/actiontext"
+
