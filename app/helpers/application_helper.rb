57e0c916b20789e1b970007e5de45bb3be3e3cd1
Mohamed A. Negm <mr.negm90@gmail.com>
2022-01-10 20:46:21 -0800

    application helper name + badge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def name(user_id)
+        user = User.find(user_id)
+        full_name = user.first_name + " " + user.last_name
+        if user.admin
+            bage = content_tag(:i, "", class: ["bi", "bi-patch-check-fill"], style: ["font-size: 1.2rem;", "color: orange;"])
+            name = content_tag(:span, full_name, title:"Admin")
+            return bage + " " + name
+        elsif user.verified
+            bage = content_tag(:i, "", class: ["bi", "bi-patch-check-fill"], style: ["font-size: 1.2rem;", "color: cornflowerblue;"]) 
+            name = content_tag(:span, full_name, title:"Verified")
+            return bage + " " + name
+        end
+        return full_name 
+    end
 end
+
+
