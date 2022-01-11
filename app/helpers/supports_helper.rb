78ac88527eddcd4a05b9c4f324d5ebfc039f1d10
Mohamed A. Negm <mr.negm90@gmail.com>
2022-01-10 21:10:57 -0800

    logic and bootstrap for support and show page status bar

--- app/helpers/supports_helper.rb
+++ app/helpers/supports_helper.rb
@@ -1,2 +1,12 @@
 module SupportsHelper
+    def status(support_id)
+        support = Support.find(support_id)
+        if support.status == "Resolved"
+            return badge = content_tag(:i, " #{support.status}" , class: ["bi", "bi-check-circle-fill"], style: ["font-size: 1.2rem;", "color: green;"])  
+        elsif support.status == "Rejected"
+            return badge = content_tag(:i, " #{support.status}" , class: ["bi", "bi-x-circle-fill"], style: ["font-size: 1.2rem;", "color: red;"])   
+        else
+           return badge = content_tag(:i, " #{support.status}" , class: ["bi", "bi-pause-circle-fill"], style: ["font-size: 1.2rem;", "color: orange;"])                       
+        end
+    end
 end
